7096fc44a462a432abe1031df5fda1a4da8f7720
Roman <Roman22091995@yandex.ru>
2023-07-10 22:23:47 +0300

    update (commit and push all)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Comparison of HelloWorld beans: " + (helloWorldBean1 == helloWorldBean2));
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Comparison of Cat beans: " + (catBean1 == catBean2));
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,13 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setName("Whiskers");
+        cat.setAge(3);
+        cat.setBreed("Persian");
+        return cat;
+    }
 }
